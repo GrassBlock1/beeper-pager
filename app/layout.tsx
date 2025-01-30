92126a0b714026f217791ab98bb5710a3da690e5
grassblock <hi@imgb.space>
2025-01-30 17:33:55 +0800

    feat: add auto dark/light modes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import type React from "react"
+import { ThemeProvider } from "@/components/theme-provider"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -22,11 +23,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
-        {process.env.NODE_ENV === "development" ? null :
-        <script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>
-        }
-        <a href={`https://${process.env.NTFY_HOST}/${process.env.NTFY_TOPIC}`} className="absolute top-0 right-0 p-4 text-sm text-gray-500 underline">View Messages</a> | <a href={`https://github.com/GrassBlock1/beeper-pager`} className="absolute top-0 left-0 p-4 text-sm text-gray-500 underline">Source code</a>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            {children}
+            {process.env.NODE_ENV === "development" ? null :
+            <script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>
+            }
+            <a href={`https://${process.env.NTFY_HOST}/${process.env.NTFY_TOPIC}`} className="absolute top-0 right-0 p-4 text-sm text-gray-500 underline">View Messages</a> | <a href={`https://github.com/GrassBlock1/beeper-pager`} className="absolute top-0 left-0 p-4 text-sm text-gray-500 underline">Source code</a>
+      </ThemeProvider>
       </body>
     </html>
   )
